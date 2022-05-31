5d8edc1e24f4ccfb258ceda45a7147aa51126494
Hugo Boutot <hugo.anthonyboutot@gmail.com>
2022-05-31 21:52:47 +0200

    finish call

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import contract from './contracts/NFTCollectible.json';
+import { ethers } from 'ethers';
 
-const contractAddress = "0x456B834F5F3Df0fEB74E969821cd198E4e79a77f"
-const abi = contract.abi;
+
+const contractAddress = "0x424BC911D6D5f8fD24337318deab93E004716c11"
+const abi = contract;
 
 function App() {
   const [currentAccount, setCurrentAccount] = useState(null);
@@ -11,6 +13,7 @@ function App() {
 
   const checkWalletIsConnected = async () => {
     const { ethereum } = window;
+    console.log(window);
 
     if (!ethereum) {
       console.log("Make sure u haze Metamask installed");
@@ -46,7 +49,30 @@ function App() {
     }
   }
 
-  const mintNftHandler = () => { }
+  const mintNftHandler = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        console.log(abi);
+        const nftContract = new ethers.Contract(contractAddress, abi, signer);
+        console.log(nftContract);
+
+        console.log("Start Airdrop");
+        // let nftTxn = await nftContract.mintNFTs(1, { value: ethers.utils.parseEther("0.01") });
+        let nftTxn = await nftContract.airdrop("0x06597ee1c914EF04a4DC77eD305C264Fb4B9899B", 1);
+        console.log("Mining... please wait");
+        await nftTxn.wait();
+        console.log(`Done: ${nftTxn.hash}`);
+      } else {
+        console.log("ETH OBJ does not exist");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
   const connectWalletButton = () => {
     return (
